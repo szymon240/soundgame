94282a6a7fece9ff79a99b4f5e076b595e005803
Adam Czyżak <101229929+VadamC27@users.noreply.github.com>
2025-01-19 14:47:41 +0100

    Version v0.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 27
         targetSdk = 34
         versionCode = 1
-        versionName = "0.2.2"
+        versionName = "0.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resValue(
